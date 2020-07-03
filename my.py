25f8c772ba42afc388d4d756f65bc0ba2494c36c
root <root@ip-172-31-5-30.ap-south-1.compute.internal>
2020-07-03 13:32:54 +0000

    update

---
+++ my.py
@@ -0,0 +1,15 @@
+i=1
+while i<=10:
+    print(F"help {i}")
+    i=i+1
+
+# now we will take sum 
+total =0
+i = 1
+
+while i<=10 :
+     
+    total  += i    # herer totao is zero but it incer by i 
+
+    i += 1          # this is same as i = i + 1
+
